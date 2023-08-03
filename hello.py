c672f4e4aa97df66da675b89f134c4dc92ca9fd8
Jesse Oseafiana <118069125+t8s1n@users.noreply.github.com>
2023-08-02 22:44:17 -0400

    goodbye

--- hello.py
+++ hello.py
@@ -1,1 +1,9 @@
-print('hello darkness my old friend')
+def hello():
+    print('hello darkness my old friend...')
+
+def goodbye():
+    print("ive come to see you yet again...")
+
+hello()
+goodbye()
+
